576a5d30e6937927a94f0f523d71e5861b7996bb
Joon Park <joon.park13@gmail.com>
2018-05-31 13:39:37 -0500

    Populate Occupied vs. Vacant Housing topic

--- censusreporter.py
+++ censusreporter.py
@@ -7,7 +7,7 @@ def render(table, params):
 
 
     TOPIC_KEYS = ['B01001', 'B01001', 'B03002', 'B19001', 'B17001', 'B08006',
-        'B11002', 'B12001', 'B13016']
+        'B11002', 'B12001', 'B13016', 'B25002']
     STATE_FIPS = ["01", "02", "04", "05", "06", "08", "09", "10", "11", "12",
         "13", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
         "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35",
@@ -218,6 +218,10 @@ def render(table, params):
             curated_data.insert(7, '40 to 44', data['B13016008'])
             curated_data.insert(8, '45 to 50', data['B13016009'])
 
+        elif topic_num == 9: # Occupied vs. Vacant Housing
+            curated_data.insert(2, 'Occupied', data['B25002002'])
+            curated_data.insert(3, 'Vacant', data['B25002003'])
+
         return curated_data
 
 
@@ -247,5 +251,5 @@ def render(table, params):
 
 
 if __name__ == "__main__":
-    dframe = render(None, {'topic': 8, 'sumlevel': 1, 'states-for-counties': 0})
+    dframe = render(None, {'topic': 9, 'sumlevel': 1, 'states-for-counties': 0})
     print(dframe)
